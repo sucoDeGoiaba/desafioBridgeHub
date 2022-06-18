98e9034bb90d259244609b33c75fd62656f997cb
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-18 07:48:07 -0300

    implementando rota /add_user/

--- app.py
+++ app.py
@@ -33,5 +33,16 @@ def user_id(id):
     return jsonify(usuario)
     
 
+@app.route('/add_user/', methods=['POST'])
+def add_user():
+    con = db_conexao()
+    nome = request.json['nome']
+    email = request.json["email"]
+    telefone = request.json["telefone"]
+
+    query = con.execute('INSERT INTO usuarios (nome, email, telefone) VALUES (?, ?, ?)', (nome, email, telefone))
+    con.commit()
+    return jsonify(query.lastrowid)
+
 if __name__ == '__main__':
     app.run(debug=True)
