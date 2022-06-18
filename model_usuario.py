15a3eaed19eed4dd234784801c62e1a2d3d47ef1
sucoDeGoiaba <flp.coding@gmail.com>
2022-06-18 11:23:49 -0300

    removendo código unitilizado

--- model_usuario.py
+++ model_usuario.py
@@ -1,5 +1,4 @@
 import sqlite3
-from schema_usuario import SchemaUsuario
 from dao_usuario import DaoUsuario
 
 
--- schema_usuario.py
+++
@@ -1,5 +0,0 @@
-class SchemaUsuario:
-    def __init__(self, nome, email, telefone):
-        self.nome = nome
-        self.email = email
-        self.telefone = telefone
